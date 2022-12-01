9baecca2cb31ae1913ef0ca3bc9d6b052440ffba
2585337885 <2585337885@qq.com>
2022-12-01 17:19:25 +0800

    commit

---
+++ src/main/java/com/sz/service/BookService.java
@@ -0,0 +1,14 @@
+package com.sz.service;
+
+import com.sz.pojo.Books;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface BookService {
+    int addBook(Books books);
+    int deleteBookById(int id);
+    int updateBook(Books books);
+    Books queryBookById(int id);
+    List<Books> queryAllBook();
+}
---
+++ src/main/java/com/sz/service/BookServiceImpl.java
@@ -0,0 +1,37 @@
+package com.sz.service;
+
+import com.sz.dao.BookMapper;
+import com.sz.pojo.Books;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class BookServiceImpl implements BookService{
+@Autowired
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
